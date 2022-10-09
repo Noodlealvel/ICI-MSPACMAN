0daec826bdb565be17c2bb4ab59e176231b5158e
Noodlealvel <alvelasco2001@gmail.com>
2022-10-09 20:13:17 +0200

    Update Prac1Test.java

--- src/main/java/es/ucm/fdi/ici/c2223/practica1/grupo04/Prac1Test.java
+++ src/main/java/es/ucm/fdi/ici/c2223/practica1/grupo04/Prac1Test.java
@@ -12,7 +12,7 @@ public class Prac1Test {
 		 .setScaleFactor(3.0)
 		 .setSightLimit(0)
 		 .build();
-		 PacmanController pacMan = new es.ucm.fdi.ici.c2122.practica1.grupo01.MsPacMan();
+		 PacmanController pacMan = new MsPacMan();
 		 GhostController ghosts = new Ghosts();
 
 		 System.out.println(
