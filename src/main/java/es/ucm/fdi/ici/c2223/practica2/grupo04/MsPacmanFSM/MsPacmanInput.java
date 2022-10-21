72d5313bcb8a281e7cea8657e5e294eeb5b7530a
Noodlealvel <alvelasco2001@gmail.com>
2022-10-21 15:35:12 +0200

    Avance en input chapucero

--- src/main/java/es/ucm/fdi/ici/c2223/practica2/grupo04/MsPacmanFSM/MsPacmanInput.java
+++ src/main/java/es/ucm/fdi/ici/c2223/practica2/grupo04/MsPacmanFSM/MsPacmanInput.java
@@ -1,14 +1,32 @@
 package es.ucm.fdi.ici.c2223.practica2.grupo04.MsPacmanFSM;
 
+import java.util.Map;
+
 import es.ucm.fdi.ici.Input;
+import pacman.game.Constants.GHOST;
 import pacman.game.Game;
 
 public class MsPacmanInput extends Input{
 
-	//Variables para almacenar informacion del game
+	//Variables para almacenar informacion del game y saber cuando transicionar
 	
-	private boolean ghostClose;
+	private final static int ghostClose = 15;
+	private final static int ghostCloseMedium = 30;
+	private final static int PPDistance = 90;
+	private boolean ghostsTogether;
+	private boolean ghostsApart;
+	private boolean edibleGhostClose;
+	private Map <GHOST,Integer> ghostEdibleTimes;
 	private boolean PPeaten;
+	private boolean noPPsleft;
+	private boolean lotsofPPinZone;
+	private boolean levelchange;
+	private boolean ghostInPath;
+	private int nextScore;
+	private boolean multipleGhostsClose;
+	
+	
+	
 	
 	
 	public MsPacmanInput(Game game) {
