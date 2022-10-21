31b192e64916bfafb488abaeef9a075c18a23bfb
Marta Gómez <martagomezs@correo.ugr.es>
2022-10-21 14:53:11 +0200

    Update GhostsFleeFromPPillAction.java

--- src/main/java/es/ucm/fdi/ici/c2223/practica2/grupo04/GhostsFSM/GhostsFleeFromPPillAction.java
+++ src/main/java/es/ucm/fdi/ici/c2223/practica2/grupo04/GhostsFSM/GhostsFleeFromPPillAction.java
@@ -1,20 +1,24 @@
 package es.ucm.fdi.ici.c2223.practica2.grupo04.GhostsFSM;
 
 import es.ucm.fdi.ici.Action;
+import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
 
 public class GhostsFleeFromPPillAction implements Action {
 
+	GHOST ghost;
+	public GhostsFleeFromPPillAction( GHOST ghost) {
+		this.ghost = ghost;
+	}
+	
 	@Override
 	public String getActionId() {
-		// TODO Auto-generated method stub
-		return null;
+		return "GhostsFleeFromPPillAction";
 	}
 
 	@Override
 	public MOVE execute(Game game) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
