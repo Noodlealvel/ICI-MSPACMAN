532e0de1a0db068a654d48e2d8da6da4346cbdbe
Pablo Pardos <101341441+PaPardos@users.noreply.github.com>
2022-10-26 11:26:00 +0200

    Ghost Eaten Transition

--- src/main/java/es/ucm/fdi/ici/c2223/practica2/grupo04/GhostsFSM/GhostsInput.java
+++ src/main/java/es/ucm/fdi/ici/c2223/practica2/grupo04/GhostsFSM/GhostsInput.java
@@ -12,6 +12,10 @@ public class GhostsInput extends Input {
 	private boolean sueEdible;
 	private int currentLevel;
 	private boolean eatenPPill;
+	private boolean blinkyEaten;
+	private Boolean pinkyEaten;
+	private Boolean sueEaten;
+	private boolean inkyEaten;
 	static private int storedLevel = -1;
 	public GhostsInput(Game game) {
 		super(game);
@@ -20,9 +24,13 @@ public class GhostsInput extends Input {
 	public void parseInput() {
 		eatenPPill = game.wasPowerPillEaten();
 		currentLevel = game.getCurrentLevel();
+		blinkyEaten = game.wasGhostEaten(GHOST.BLINKY);
 		blinkyEdible = game.isGhostEdible(GHOST.BLINKY);
+		pinkyEaten = game.wasGhostEaten(GHOST.PINKY);
 		pinkyEdible = game.isGhostEdible(GHOST.PINKY);
+		sueEaten = game.wasGhostEaten(GHOST.SUE);
 		sueEdible = game.isGhostEdible(GHOST.SUE);
+		inkyEaten = game.wasGhostEaten(GHOST.INKY);
 		inkyEdible = game.isGhostEdible(GHOST.INKY);
 	}
 	public boolean isGhostEdible(GHOST ghost) {
@@ -48,6 +56,20 @@ public class GhostsInput extends Input {
 	}
 	public boolean PPillEaten() {
 		return eatenPPill;
+	}
+	public boolean wasGhostEaten(GHOST ghost) {
+		switch(ghost) {
+		case BLINKY:
+			return blinkyEaten;
+		case INKY:
+			return inkyEaten;
+		case PINKY:
+			return pinkyEaten;
+		case SUE:
+			return sueEaten;
+		default:
+			return false;
+	}
 	}
 
 }
---
+++ src/main/java/es/ucm/fdi/ici/c2223/practica2/grupo04/GhostsFSM/Transitions/GhostsEatenTransition.java
@@ -0,0 +1,27 @@
+package es.ucm.fdi.ici.c2223.practica2.grupo04.GhostsFSM.Transitions;
+
+import es.ucm.fdi.ici.Input;
+import es.ucm.fdi.ici.c2223.practica2.grupo04.GhostsFSM.GhostsInput;
+import es.ucm.fdi.ici.fsm.Transition;
+import pacman.game.Constants.GHOST;
+
+public class GhostsEatenTransition implements Transition{
+
+	GHOST ghost;
+	public GhostsEatenTransition(GHOST ghost) {
+		super();
+		this.ghost = ghost;
+	}
+	
+	@Override
+	public boolean evaluate(Input in) {
+		GhostsInput input = (GhostsInput)in;
+		return input.wasGhostEaten(ghost);
+	}
+	
+	@Override
+	public String toString() {
+		return "Ghost was eaten";
+	}
+}
+
