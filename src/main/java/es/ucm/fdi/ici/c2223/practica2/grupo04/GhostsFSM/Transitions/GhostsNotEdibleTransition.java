b1fb632c6a27e5e84efa4f27393170bf5d0c92c5
Pablo Pardos <101341441+PaPardos@users.noreply.github.com>
2022-10-27 20:55:39 +0200

    Create GhostsNotEdibleTransition.java

---
+++ src/main/java/es/ucm/fdi/ici/c2223/practica2/grupo04/GhostsFSM/Transitions/GhostsNotEdibleTransition.java
@@ -0,0 +1,27 @@
+package es.ucm.fdi.ici.c2223.practica2.grupo04.GhostsFSM.Transitions;
+
+import es.ucm.fdi.ici.Input;
+import es.ucm.fdi.ici.c2223.practica2.grupo04.GhostsFSM.GhostsInput;
+import es.ucm.fdi.ici.fsm.Transition;
+import pacman.game.Constants.GHOST;
+
+public class GhostsNotEdibleTransition implements Transition {
+
+	GHOST ghost;
+	public GhostsNotEdibleTransition(GHOST ghost) {
+		super();
+		this.ghost = ghost;
+	}
+	
+	@Override
+	public boolean evaluate(Input in) {
+		GhostsInput input = (GhostsInput)in;
+		return !input.isGhostEdible(ghost);
+	}
+	
+	@Override
+	public String toString() {
+		return ghost.toString() + " is no longer edible";
+	}
+
+}
