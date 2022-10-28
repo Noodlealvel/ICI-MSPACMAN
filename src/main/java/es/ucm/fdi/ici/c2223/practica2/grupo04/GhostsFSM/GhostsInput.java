e4e4762dd12d1901c463ddba1b3fd0be1264d0c0
Pablo Pardos <papardos@ucm.es>
2022-10-28 14:36:03 +0200

    Transición persecución cercana

--- src/main/java/es/ucm/fdi/ici/c2223/practica2/grupo04/GhostsFSM/GhostsInput.java
+++ src/main/java/es/ucm/fdi/ici/c2223/practica2/grupo04/GhostsFSM/GhostsInput.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import es.ucm.fdi.ici.Input;
 import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
+import pacman.game.Constants.MOVE;
 import pacman.game.Game;
 import es.ucm.fdi.ici.c2223.practica2.grupo04.GhostsFSM.GhostsUtils;
 
@@ -18,6 +19,8 @@ public class GhostsInput extends Input {
 	private boolean pacmanNearPPill;
 	private GHOST nearestGhostToPacman;
 	private boolean noPPills;
+	private HashMap<GHOST,MOVE> ghostsLastMovementMade;
+	private MOVE pacmanLastMovement;
 	private static final int SECURITY_DISTANCE = 60;
 	private static final int LOW_TIME = 10;
 	private static final int CLOSE_PACMAN_DISTANCE = 40;
@@ -27,6 +30,7 @@ public class GhostsInput extends Input {
 	@Override
 	public void parseInput() {
 		ghostsInfoMap = new HashMap<GHOST,HashMap<GhostsRelevantInfo, Boolean>>();
+		ghostsLastMovementMade = new HashMap<GHOST,MOVE>();
 		for (GHOST ghost : GHOST.values()) {
 			HashMap<GhostsRelevantInfo, Boolean> ghostMap = new HashMap<GhostsRelevantInfo, Boolean>();
 			ghostMap.put(GhostsRelevantInfo.EATEN, game.wasGhostEaten(ghost));
@@ -36,10 +40,12 @@ public class GhostsInput extends Input {
 			ghostMap.put(GhostsRelevantInfo.GHOSTS_CLOSE, GhostsUtils.GhostCloseToRest(game,ghost));
 			ghostMap.put(GhostsRelevantInfo.LOW_EDIBLE_TIME, game.getGhostEdibleTime(ghost) <= LOW_TIME);
 			ghostMap.put(GhostsRelevantInfo.PACMAN_CLOSE, game.getDistance(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(ghost), game.getPacmanLastMoveMade() ,DM.PATH) <= CLOSE_PACMAN_DISTANCE);
-			ghostMap.put(GhostsRelevantInfo.NO_GHOSTS_IN_PATH, GhostsUtils.PathContainsGhosts(game, game.getShortestPath(game.getGhostCurrentNodeIndex(ghost), game.getPacmanCurrentNodeIndex(), game.getGhostLastMoveMade(ghost), DM.PATH)));
+			ghostMap.put(GhostsRelevantInfo.NO_GHOSTS_IN_PATH, GhostsUtils.PathContainsGhosts(game, game.getShortestPath(game.getGhostCurrentNodeIndex(ghost), game.getPacmanCurrentNodeIndex(), game.getGhostLastMoveMade(ghost))));
 			ghostsInfoMap.put(ghost, ghostMap);
+			ghostsLastMovementMade.put(ghost, game.getGhostLastMoveMade(ghost));
 			
 		}
+		pacmanLastMovement = game.getPacmanLastMoveMade();
 		noPPills = game.getNumberOfActivePowerPills() > 0;
 		eatenPPill = game.wasPowerPillEaten();
 		currentLevel = game.getCurrentLevel();
@@ -105,6 +111,9 @@ public class GhostsInput extends Input {
 	}
 	public boolean ghostsInPath(GHOST ghost){
 		return !ghostsInfoMap.get(ghost).get(GhostsRelevantInfo.NO_GHOSTS_IN_PATH);
+	}
+	public boolean sameLastMovement(GHOST ghost) {
+		return ghostsLastMovementMade.get(ghost) == pacmanLastMovement;
 	}
 }
 
---
+++ src/main/java/es/ucm/fdi/ici/c2223/practica2/grupo04/GhostsFSM/Transitions/GhostsJustBehindPacmanTransition.java
@@ -0,0 +1,27 @@
+package es.ucm.fdi.ici.c2223.practica2.grupo04.GhostsFSM.Transitions;
+
+import es.ucm.fdi.ici.Input;
+import es.ucm.fdi.ici.c2223.practica2.grupo04.GhostsFSM.GhostsInput;
+import es.ucm.fdi.ici.fsm.Transition;
+import pacman.game.Constants.GHOST;
+
+public class GhostsJustBehindPacmanTransition implements Transition {
+
+	GHOST ghost;
+	public GhostsJustBehindPacmanTransition(GHOST ghost) {
+		super();
+		this.ghost = ghost;
+	}
+	
+	@Override
+	public boolean evaluate(Input in) {
+		GhostsInput input = (GhostsInput)in;
+		return input.nearToPacman(ghost) && input.sameLastMovement(ghost);
+	}
+	
+	@Override
+	public String toString() {
+		return ghost.toString() + "is just behind pacman.";
+	}
+
+}
