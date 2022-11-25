775187efdb3cad23b1d3f53e944785b32c6cf24e
Pablo Pardos <papardos@ucm.es>
2022-11-25 14:08:34 +0100

    Esqueleto prac4

---
+++ src/main/java/es/ucm/fdi/ici/c2223/practica4/grupo04/Ghosts.java
@@ -0,0 +1,18 @@
+package es.ucm.fdi.ici.c2223.practica4.grupo04;
+
+import java.util.EnumMap;
+
+import pacman.controllers.GhostController;
+import pacman.game.Constants.GHOST;
+import pacman.game.Constants.MOVE;
+import pacman.game.Game;
+
+public class Ghosts extends GhostController {
+
+	@Override
+	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/es/ucm/fdi/ici/c2223/practica4/grupo04/MsPacMan.java
@@ -0,0 +1,15 @@
+package es.ucm.fdi.ici.c2223.practica4.grupo04;
+
+import pacman.controllers.PacmanController;
+import pacman.game.Constants.MOVE;
+import pacman.game.Game;
+
+public class MsPacMan extends PacmanController {
+
+	@Override
+	public MOVE getMove(Game game, long timeDue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
