808abb6faaff2eef78d9bad3fdb1802f2869299f
Noodlealvel <alvelasco2001@gmail.com>
2022-12-02 15:37:42 +0100

    Memoria a

--- src/main/java/es/ucm/fdi/ici/c2223/practica4/grupo04/MsPacManFuzzyMemory.java
+++ src/main/java/es/ucm/fdi/ici/c2223/practica4/grupo04/MsPacManFuzzyMemory.java
@@ -2,4 +2,6 @@ package es.ucm.fdi.ici.c2223.practica4.grupo04;
 
 public class MsPacManFuzzyMemory {
 
+	
+	//GUARDAR DISTANCIAS Y POSICIONES PARA USARLAS EN LAS ACCIONES PARA COMER A PINKY O HUIR O YOKSE
 }
